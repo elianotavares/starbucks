74f06aa125c0787d663cb697c08c7d69ab959d9b
Eliano3 <elianoricardo53@gmail.com>
2026-03-15 16:50:07 +0100

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+/*alert("Hello World!");*/
+
+
+let nome = "Eliano"; 
+
+console.log(nome); /* exibe no console do navegador */
+
+function genteBoa() {
+    console.log("Eliano é gente boa!");
+
+}
+
+genteBoa ()
+
+
+let circulo = document.querySelector(".circulo")
+
+function mudarCor(cor) {
+
+    console.log(cor);
+    circulo.style.backgroundColor = cor
+
+}
